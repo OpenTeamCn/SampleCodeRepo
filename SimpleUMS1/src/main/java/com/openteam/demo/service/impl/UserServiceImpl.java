7f5686d0018666cb78829f54c5091f0eead12518
winjerry <369967329@qq.com>
2013-09-06 13:39:32 +0800

    test for list

--- SimpleUMS1/src/main/java/com/openteam/demo/service/impl/UserServiceImpl.java
+++ SimpleUMS1/src/main/java/com/openteam/demo/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.openteam.demo.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.openteam.demo.bean.UserBean;
 import com.openteam.demo.dao.UserDao;
 import com.openteam.demo.service.UserService;
-import java.util.List;
+import com.openteam.demo.utils.DateUtil;
 
 public class UserServiceImpl implements UserService {
 	private UserDao userDao;
@@ -17,7 +21,16 @@ public class UserServiceImpl implements UserService {
 	}
 
 	public List<UserBean> getAllUser() {
-		return null;
+		List<UserBean> userList = new ArrayList<UserBean>();
+		for(int i=0 ;i < 5 ; i++) {
+			UserBean bean = new UserBean();
+			bean.setBirthday(DateUtil.getDateStrDay(new Date()));
+			bean.setId(new Long(i));
+			bean.setUid(new Long(100+i));
+			bean.setPhoto("https://www.baidu.com/sample/test.image");
+			userList.add(bean);
+		}
+		return userList;
 	}
 
 	public Long saveUser(UserBean bean) {
